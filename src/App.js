214ad6e9b3fd899a5f5421482cceae44450c10da
Yauheniya <ya.kavalchuk@gmail.com>
2024-04-04 15:28:04 +0300

    fix mistakes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import avatar from "./components/Avatar/avatar";
+import React from "react";
+import { Avatar } from "./components/Avatar/Avatar";
 
 class App extends React.Component {
-avatarList = [
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatarList: [
         {
-            imageSrc: "//https://i.imgur.com/1bX5QH6.jpg",
-            userName: "Lin Lanying"
+          imageSrc: "https://i.imgur.com/1bX5QH6.jpg",
+          userName: "Lin Lanying",
         },
         {
-            imageSrc: "https://i.imgur.com/YfeOqp2.jpg",
-            userName: "Test user"
-        }
-    ]
-render () {
-    return (
-        <div className="App">
-            {avatarList.map(() => <avatar userName={userName} imageSrc={imageSrc} />)}
-        </div>
-    );
-}
+          imageSrc: "https://i.imgur.com/YfeOqp2.jpg",
+          userName: "Test user",
+        },
+      ],
+    };
+  }
 
+  render() {
+    return (
+      <div className="App">
+        {this.state.avatarList.map((avatar) => (
+          <Avatar userName={avatar.userName} imageSrc={avatar.imageSrc} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
